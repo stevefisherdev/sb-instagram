ab19ff31ec1367a7210b30b8ed2f49593f090624
Steve Fisher <60248912+stevefisherdev@users.noreply.github.com>
2022-04-04 12:10:29 -0700

    Adds support for embedding reels

--- src/control/util/get-embed-id.ts
+++ src/control/util/get-embed-id.ts
@@ -1,5 +1,5 @@
 export const getEmbedId = (url: string) => {
-  const idPattern = /(?:instagram.com\/(?:p|tv)\/([A-Za-z0-9_-]{11}))/i;
+  const idPattern = /(?:instagram.com\/(?:p|tv|reel)\/([A-Za-z0-9_-]{11}))/i;
   const matches = idPattern.exec(url);
   return matches && matches[1];
 };
